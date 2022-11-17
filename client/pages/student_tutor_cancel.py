111d7c318b4341f420b1eebd5261b12d4c0132c5
ErnestM1234 <73407149+ErnestM1234@users.noreply.github.com>
2022-11-17 15:49:15 -0500

    feat: add student dashboard
    * unsure
    
    * prototype
    
    * prototype
    
    * feat: add user CRUD and param validation
    
    * add tutorship api crud
    
    * feat: add tutorcourse api and change model to courses
    
    * Beginning of alpha development
    
    * Anjali's frontend changes
    
    * Fixed API syntax errors; allowed manual tutorship creation on homepage
    
    * feat: add admin dashboard
    
    * Generate courses & tutorships on homepage
    
    * Alpha temp branch (#11)
    
    * fix api
    
    * small fix
    
    * Edits to dashboard
    
    * changes to tutor dash
    
    * Pre-merge save
    
    * feat: add admin functionalities
    
    * add admin
    
    * add more functionality
    
    * added student and tutor profiles
    
    * not sure
    
    * tutor changes
    
    * changes
    
    * connected to backend
    
    * Add deleting functionality and other things
    
    * add admin
    
    * add more functionality
    
    * added student and tutor profiles
    
    * not sure
    
    * feat: add course removal
    
    * feat: add dissolve tutorship
    
    * add profile links
    
    * fixed confirm page
    
    * "Student flow"
    
    * Linked student sidebars to tutor dashboard; ideally replaced by single imported sidebar file
    
    * app.run
    
    Co-authored-by: Lana Glisic <lg@dynamic-oit-vapornet100-10-8-33-111.princeton.edu>
    Co-authored-by: lglisic <lglisic@princeton.edu>
    Co-authored-by: akkapoor14 <akkapoor@princeton.edu>

---
+++ client/pages/student_tutor_cancel.py
@@ -0,0 +1,40 @@
+
+import os
+from dotenv import load_dotenv
+from app import app
+import requests
+from flask import redirect, render_template, request
+
+load_dotenv()
+
+@app.route('/student/tutor/cancel')
+def student_tutor_cancel():
+
+    # TO CHANGE
+    course_id = request.args.get('course_id')
+    tutor_id = request.args.get('tutor_id')
+
+    # this is temporary, this will be given to us by CAS or smth
+    userId = 1
+    
+    # get student
+    res = requests.get(url = str(os.environ['API_ADDRESS']+'/api/user/'), params={"id": userId})
+    user = res.json()
+    # verify is student
+    if "id" not in user.keys() or user['is_student'] == False:
+        return redirect('/')
+
+    # get course
+    res = requests.get(url = str(os.environ['API_ADDRESS']+'/api/course/'), params={"id": course_id})
+    course = res.json()
+
+    # get tutor
+    res = requests.get(url = str(os.environ['API_ADDRESS']+'/api/user/'), params={"id": tutor_id})
+    tutor = res.json()
+
+    return render_template(
+        'student-tutor-cancel.html',
+        user=user,
+        course=course,
+        tutor=tutor
+    )
