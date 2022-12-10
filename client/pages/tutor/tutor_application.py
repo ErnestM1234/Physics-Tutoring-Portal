7d881557cc27b87d32988395b4cb6d50b94b6e97
akkapoor14 <akkapoor@princeton.edu>
2022-12-10 16:09:57 -0500

    changing tutor application

--- client/pages/tutor/tutor_application.py
+++ client/pages/tutor/tutor_application.py
@@ -2,9 +2,8 @@ import os
 from dotenv import load_dotenv
 from app import app
 import requests
-from flask import render_template
+from flask import redirect, render_template, request
 from pages.shared.get_user import *
-
 
 load_dotenv()
 
@@ -20,13 +19,21 @@ def tutorApplication():
     # get headers
     headers = get_header()
 
-    
-    res = requests.get(url = str(os.environ['API_ADDRESS']+'/api/courses/'), headers=headers)
-    courses = res.json()
+    course_id = request.args.get('course_id')
+    print(course_id)
+    res = requests.get(url = str(os.environ['API_ADDRESS']+'/api/course/'), params={'id': course_id}, headers=headers)
+    course = res.json()
 
-    res = requests.get(url = str(os.environ['API_ADDRESS']+'/api/user/'), params={'id': user['id']}, headers=headers)
-    tutor = res.json()
+
+
+    
+    #res = requests.get(url = str(os.environ['API_ADDRESS']+'/api/courses/'), headers=headers)
+    #courses = res.json()
+
+    #res = requests.get(url = str(os.environ['API_ADDRESS']+'/api/user/'), params={'id': user['id']}, headers=headers)
+    #tutor = res.json()
 
     return render_template(
-        '/tutor/tutor-application.html', courses=courses, tutor=tutor
+        '/tutor/tutor-application.html', course = course
+
     )
